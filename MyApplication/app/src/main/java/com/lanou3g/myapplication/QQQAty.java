0c67102e6f7bf1e7d61a48e58def5c0a96b43f5f
qinqianqian <415807297@qq.com>
2016-06-12 17:27:32 +0800

    qqq 修改 qqqAty

---
+++ MyApplication/app/src/main/java/com/lanou3g/myapplication/QQQAty.java
@@ -0,0 +1,16 @@
+package com.lanou3g.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 秦谦谦 on 16/6/12 17:24.
+ */
+public class QQQAty extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aty_qqq);
+    }
+}
